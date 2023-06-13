833d4faba275f6d6d67fa2602f178c3aa5035ab5
Manjeet7087 <131547570+Manjeet7087@users.noreply.github.com>
2023-06-13 21:44:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function threeSum(arr, target) {
 }
 
 // Example usage:
-console.log(decimalToBinary(7)); // Output: 111
-console.log(decimalToBinary(10)); // Output: 1010
-console.log(decimalToBinary(33)); // Output: 100001
+//console.log(decimalToBinary(7)); // Output: 111
+//console.log(decimalToBinary(10)); // Output: 1010
+//console.log(decimalToBinary(33)); // Output: 100001
 
   
 }
